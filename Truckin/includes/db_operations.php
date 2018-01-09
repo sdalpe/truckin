c68bcd71803b0bf32a356513c542a47369b12cf1
mrspinn <34923439+mrspinn@users.noreply.github.com>
2018-01-09 08:59:52 -0500

    Add files via upload

--- Truckin/includes/db_operations.php
+++ Truckin/includes/db_operations.php
@@ -2,8 +2,8 @@
 function dbconnect($host, $db, $user, $pw){
 $db_conn = new mysqli($host, $user, $pw, $db);
 	if ($db_conn->connect_errno) {
-   	 die ("Could not connect to database server".$db_host."\n Error: "
-			.$db_conn->connect_errno 
+   	 die ("Could not connect to database server".$host."\n Error: "
+			.$db_conn->connect_errno
 			."\n Report: ".$db_conn->connect_error."\n");
 	}
 	return $db_conn;
